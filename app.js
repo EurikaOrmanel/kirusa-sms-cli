a89afb92d469fa109e6f623aa8f5b58c5d101168
EurikaProg201 <wworgba@gmail.com>
2021-07-25 14:41:13 +0000

    tweak

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const inquirer = require('inquirer');
 const { sendSMS } = require('./lib/sender');
 const { query_for_new_login_details, query_for_existing_data } = require('./lib/query')
-const { csvWriter }= require('./lib/csv-writer');
+const { csvWriter } = require('./lib/csv-writer');
 inquirer.prompt([{
     type: 'confirm',
     name: 'old',
-    message: "Do you want to use the previous authentication detials?",
+    message: "Do you want to use the previous authentication details?",
     default: false,
-}
-]).then(res => {
+}]).then(res => {
     if (res.old === true) {
         inquirer.prompt(query_for_existing_data).then(res => sendSMS(res.body, "[" + res.recievers.trim() + "]", res.priority))
     } else {
@@ -17,10 +16,10 @@ inquirer.prompt([{
                 account_id: res.account_id,
                 auth_key: res.auth_key
             }];
-            sendSMS(res.body, "[" + res.recievers.trim() + "]", res.priority);
             csvWriter.writeRecords(info).then(() => {
                 console.log('Previous auth details has been mutated');
-            })
+            });
+            sendSMS(res.body, "[" + res.recievers.trim() + "]", res.priority);
         });
 
     }
