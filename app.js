5ec5f15c8d9f6202e6a40d5f5615cb358a8c6741
EurikaProg201 <wworgba@gmail.com>
2021-07-26 09:45:21 +0000

    usage of configstore instead of csv

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 #!/usr/bin/env node
 'use strict';
-
-
 const inquirer = require('inquirer');
 const { sendSMS } = require('./lib/sender');
 const { query_for_new_login_details, query_for_existing_data } = require('./lib/query');
-const { csvWriter } = require('./lib/csv-writer');
-inquirer.prompt([{
-    type: 'confirm',
-    name: 'old',
-    message: "Do you want to use the previous authentication details?",
-    default: false,
-}]).then(res => {
-    if (res.old === true) {
-        inquirer.prompt(query_for_existing_data).then(res => sendSMS(res.body, "[" + res.recievers.trim() + "]", res.priority))
-    } else {
-        inquirer.prompt(query_for_new_login_details).then(res => {
-            var info = [{
-                account_id: res.account_id,
-                auth_key: res.auth_key
-            }];
-            csvWriter.writeRecords(info).then(() => {
-                console.log('Previous auth details has been mutated');
-            });
-            sendSMS(res.body, "[" + res.recievers.trim() + "]", res.priority);
-        });
+const Configstore = require('configstore');
+const pkg = require('./package.json');
+const conf = new Configstore(pkg.name);
 
-    }
-});
+if (conf.has('auth_key') == true && conf.has('auth_id')) {
+    inquirer.prompt([{
+        type: 'confirm',
+        name: 'old',
+        message: "Do you want to use the previous authentication details?",
+        default: false,
+    }]).then(res => {
+        if (res.old === true) {
+            inquirer.prompt(query_for_existing_data).then(res => sendSMS(res.body, "[" + res.recievers.trim() + "]", res.priority));
+        } else {
+            inquirer.prompt(query_for_new_login_details).then(res => {
+                conf.set('account_id', res.account_id);
+                conf.set('auth_key', res.auth_key);
+                sendSMS(res.body, "[" + res.recievers.trim() + "]", res.priority);
+            });
+
+        }
+    });
+} else {
+    inquirer.prompt([query_for_new_login_details[0], query_for_new_login_details[1]]).then(res => {
+        conf.set('account_id', res.account_id);
+        conf.set('auth_key', res.auth_key);
+    });
+}
